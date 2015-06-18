43ae2abf7ca512fb7bde5ed0a93232b882c16d91
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-06-18 15:55:02 -0300

    Created the dashboard controller

---
+++ src/System/Bundle/ManagementBundle/Controller/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace System\Bundle\ManagementBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DashboardController extends Controller
+{
+    public function indexAction()
+    {
+        echo 1;
+        exit;
+    }
+}
