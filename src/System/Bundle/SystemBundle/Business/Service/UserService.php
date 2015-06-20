7b8593d0237376148cb55c7993e659e4acb8fd82
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-06-19 23:02:19 -0300

    Changed the user service

--- src/System/Bundle/SystemBundle/Business/Service/UserService.php
+++ src/System/Bundle/SystemBundle/Business/Service/UserService.php
@@ -7,14 +7,23 @@ class UserService extends BaseService
 {
     private $em;
 
-    public function __construct()
+    public function __construct(EntityManager $em)
     {
-        
+        $this->em = $em;
     }
 
     protected function getRepository()
     {
+        return $this->em->getRepository('SystemBundle:User');
+    }
 
+    public function getByEmail($email)
+    {
+        $entity = $this->getRepository()->findOneBy(array(
+            'email' => $email
+        ));
+
+        return $entity;
     }
 
 }
