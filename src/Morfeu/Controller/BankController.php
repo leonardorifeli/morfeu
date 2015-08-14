478858989f0ecdfd50f227c5234d529b754a7a33
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-08-14 02:06:48 -0300

    Changed bankController

--- src/Morfeu/Controller/BankController.php
+++ src/Morfeu/Controller/BankController.php
@@ -6,6 +6,9 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+use Morfeu\Form\BankUserType;
+use Morfeu\Entity\BankUser;
+
 class BankController
 {
 
@@ -29,6 +32,38 @@ class BankController
         return $app['twig']->render('Bank/index.html.twig', array(
             'entities' => $result
         ));
+    }
+
+    public function newAction(Request $request, Application $app)
+    {
+        $entity = new BankUser();
+
+        $form = $this->createCreateForm($app);
+
+        return $app['twig']->render('Bank/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    public function createAction(Request $request, Application $app)
+    {
+        echo 1;
+        exit;
+    }
+
+    private function createCreateForm(Application $app)
+    {
+        $form = $app['form.factory']->createBuilder(new \Morfeu\Form\BankUserType())
+        ->setAction("bank/create")
+        ->setMethod('POST')
+        ->add('btn','submit', array(
+            'label' => "Cadastrar",
+            'attr' => array(
+                'class' => 'btn btn-success'
+            )
+        ))->getForm();
+
+        return $form;
     }
 
 }
