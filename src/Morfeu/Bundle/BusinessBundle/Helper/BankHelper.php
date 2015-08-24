06e351d4e8dc8da2defaca48b9cad76c00fe438e
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-08-23 22:48:50 -0300

    Added the helper to correct the informations

---
+++ src/Morfeu/Bundle/BusinessBundle/Helper/BankHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Morfeu\Bundle\BusinessBundle\Helper;
+
+use Morfeu\Bundle\EntityBundle\BankUser;
+use Morfeu\Bundle\BusinessBundle\Enum\Status;
+
+class BankHelper
+{
+
+    public function updateCreateDate($entity)
+    {
+        $date = new \DateTime();
+
+        $entity->setCreatedAt($date);
+        $entity->setUpdatedAt($date);
+
+        return $entity;
+    }
+
+    public function updateUser($entity, $user)
+    {
+        $entity->setUser($user);
+
+        return $entity;
+    }
+
+    public function updateStatus($entity)
+    {
+        $entity->setStatus(Status::ACTIVE);
+
+        return $entity;
+    }
+
+}
