749aeaf2f9b6cce5dc1e04ddc45b4b0535e86267
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-08-23 22:58:26 -0300

    Corrected the router and controller

--- src/Morfeu/Bundle/BankBundle/Controller/BankController.php
+++ src/Morfeu/Bundle/BankBundle/Controller/BankController.php
@@ -2,7 +2,13 @@
 
 namespace Morfeu\Bundle\BankBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Morfeu\Bundle\EntityBundle\Entity\BankUser;
+use Morfeu\Bundle\BankBundle\Form\BankUserType;
+use Morfeu\Bundle\BusinessBundle\Helper\BankHelper;
 
 class BankController extends Controller
 {
@@ -26,4 +32,54 @@ class BankController extends Controller
             'entities' => $entities
         ));
     }
-}
+
+    public function newAction()
+    {
+        $entity = new BankUser();
+        $form = $this->createCreateForm($entity);
+
+        return $this->render('BankBundle:Bank:new.html.twig', array(
+            'entity' => $entity,
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function createAction(Request $request)
+    {
+        $entity = new BankUser();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        $helper = new BankHelper();
+        $entity = $helper->updateCreateDate($entity);
+        $entity = $helper->updateUser($entity, $this->getUser());
+        $entity = $helper->updateStatus($entity);
+
+        $entity = $this->getBankUserService()->insertOrUpdate($entity);
+
+        return $this->redirect($this->generateUrl('bank'));
+    }
+
+    /**
+    * Creates a form to create a Notice entity.
+    *
+    * @param BankUser $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createCreateForm(BankUser $entity)
+    {
+        $form = $this->createForm(new BankUserType(), $entity, array(
+            'action' => $this->generateUrl('bank_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array(
+            'label' => 'Cadastrar',
+            'attr' => array(
+                'class'=>'btn btn-primary'
+            )));
+
+            return $form;
+        }
+    }
