9b276a3fab9a9536ad4ff546ac5748cd8f33cc32
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-08-24 02:29:22 -0300

    Developed the cardService for application execute some actions

---
+++ src/Morfeu/Bundle/BusinessBundle/Service/CardService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Morfeu\Bundle\BusinessBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+use Morfeu\Bundle\BusinessBundle\Service\BaseService;
+use Morfeu\Bundle\EntityBundle\Entity\Card;
+use Morfeu\Bundle\BusinessBundle\Enum\Status;
+
+class CardService extends BaseService {
+
+	public function __construct(EntityManager $em)
+	{
+		$this->em = $em;
+	}
+
+	protected function getRepository()
+	{
+		return $this->em->getRepository('EntityBundle:Card');
+	}
+
+    public function get($id)
+    {
+        $entity = $this->getRepository()->find($id);
+
+        return $entity;
+    }
+
+	public function getCardByUser($user)
+	{
+		$entities = $this->getRepository()->findBy(array(
+			'user' => $user,
+			'status' => Status::ACTIVE
+		));
+
+        return $entities;
+	}
+
+	public function insertOrUpdate($entity)
+	{
+		$result = $this->saveOrUpdate($entity);
+
+		return $result;
+	}
+
+    public function getAll()
+    {
+        $entities = $this->getRepository()->findAll();
+
+        return $entities;
+    }
+
+}
