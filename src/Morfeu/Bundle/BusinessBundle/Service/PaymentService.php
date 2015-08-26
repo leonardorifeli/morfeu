6aeb81047a147aea108bc9743a0b7ea824809731
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-08-26 18:20:06 -0300

    Developed the PaymentService

---
+++ src/Morfeu/Bundle/BusinessBundle/Service/PaymentService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Morfeu\Bundle\BusinessBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+use Morfeu\Bundle\BusinessBundle\Service\BaseService;
+use Morfeu\Bundle\EntityBundle\Entity\Payment;
+use Morfeu\Bundle\BusinessBundle\Enum\Status;
+
+class PaymentService extends BaseService {
+
+	public function __construct(EntityManager $em)
+	{
+		$this->em = $em;
+	}
+
+	protected function getRepository()
+	{
+		return $this->em->getRepository('EntityBundle:Payment');
+	}
+
+    public function get($id)
+    {
+        $entity = $this->getRepository()->find($id);
+
+        return $entity;
+    }
+
+	public function insertOrUpdate($entity)
+	{
+		$result = $this->saveOrUpdate($entity);
+
+		return $result;
+	}
+
+}
