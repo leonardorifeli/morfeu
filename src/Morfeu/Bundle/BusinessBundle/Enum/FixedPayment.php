8a7e5ff01178ccd6e07566601c42996b02ef914e
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-08-28 01:33:59 -0300

    src/Morfeu/Bundle/BusinessBundle/Enum/StatusPayment.php

---
+++ src/Morfeu/Bundle/BusinessBundle/Enum/FixedPayment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Morfeu\Bundle\BusinessBundle\Enum;
+
+use Morfeu\Bundle\BusinessBundle\Enum\BaseEnum;
+
+class FixedPayment extends BaseEnum
+{
+
+    const YES = 1;
+    const NO = 0;
+
+}
---
+++ src/Morfeu/Bundle/BusinessBundle/Enum/StatusPayment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Morfeu\Bundle\BusinessBundle\Enum;
+
+use Morfeu\Bundle\BusinessBundle\Enum\BaseEnum;
+
+class StatusPayment extends BaseEnum
+{
+
+    const FINISHED = 1;
+    const DELETED = 0;
+    const PENDING = 2;
+
+}
