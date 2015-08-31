b719774134f3029fa2057076e7e4229018f00e19
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-08-31 02:38:30 -0300

    Created a new enum for control type payment

---
+++ src/Morfeu/Bundle/BusinessBundle/Enum/TypePayment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Morfeu\Bundle\BusinessBundle\Enum;
+
+use Morfeu\Bundle\BusinessBundle\Enum\BaseEnum;
+
+class TypePayment extends BaseEnum
+{
+    const ACCOMPLISHED = 1;
+    const RECEIVED = 2;
+}
