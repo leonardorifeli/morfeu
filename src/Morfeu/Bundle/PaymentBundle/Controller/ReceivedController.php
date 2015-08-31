34cbc4e433ed1c4282b02f596947f474d73784d5
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-08-31 02:21:45 -0300

    Changed index method of received controller

--- src/Morfeu/Bundle/PaymentBundle/Controller/ReceivedController.php
+++ src/Morfeu/Bundle/PaymentBundle/Controller/ReceivedController.php
@@ -6,8 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ReceivedController extends Controller
 {
+
     public function indexAction()
     {
         return $this->render('PaymentBundle:Received:index.html.twig');
     }
+
 }
