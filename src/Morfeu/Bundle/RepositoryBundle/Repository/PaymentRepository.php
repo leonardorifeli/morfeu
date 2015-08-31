85becdb3ed93c81f16d28c55ad205f28108dee72
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-08-31 02:38:57 -0300

    Created the new method to list payment

--- src/Morfeu/Bundle/RepositoryBundle/Repository/PaymentRepository.php
+++ src/Morfeu/Bundle/RepositoryBundle/Repository/PaymentRepository.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\Query;
+use Morfeu\Bundle\BusinessBundle\Enum\TypePayment;
 
 class PaymentRepository extends EntityRepository{
 
@@ -13,6 +14,20 @@ class PaymentRepository extends EntityRepository{
     {
         $result =  $this->createQueryBuilder('c')
         ->orderBy('c.name', 'ASC');
+
+        return $result;
+    }
+
+    public function findAccomplishedByUser($user, $period = null)
+    {
+        $result = $this->createQueryBuilder('c')
+        ->where('c.paymentType = :type')
+        ->andWhere('c.user = :user')
+        ->setParameter(':type', TypePayment::ACCOMPLISHED)
+        ->setParameter(':user', $user)
+        ->orderBy('c.id', 'DESC')
+        ->getQuery()
+        ->getResult();
 
         return $result;
     }
