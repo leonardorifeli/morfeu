fa390f8ab04164279d295dea0141697020846c38
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-09-01 14:15:07 -0300

    Changed the received controller

--- src/Morfeu/Bundle/PaymentBundle/Controller/ReceivedController.php
+++ src/Morfeu/Bundle/PaymentBundle/Controller/ReceivedController.php
@@ -6,10 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ReceivedController extends Controller
 {
+    private $paymentService;
+
+    private function getPaymentService()
+    {
+        if(!$this->paymentService){
+            $this->paymentService = $this->get('payment.service');
+        }
+
+        return $this->paymentService;
+    }
 
     public function indexAction()
     {
-        return $this->render('PaymentBundle:Received:index.html.twig');
+        $user = $this->getUser();
+
+        $entities = $this->getPaymentService()->getReceivedByUser($user);
+
+        return $this->render('PaymentBundle:Received:index.html.twig', array(
+            'entities' => $entities
+        ));
     }
 
 }
