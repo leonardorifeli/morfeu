d1df8d0580e272b7b35a63832cbf11e077c01353
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-11-08 23:13:00 -0200

    Developed a new form - UserProfileType to use in update profile

---
+++ src/Morfeu/Bundle/UserBundle/Form/UserProfileType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Morfeu\Bundle\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserProfileType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('photo', 'file', array(
+                'required' => false,
+                'data_class' => null
+            ))
+            ->add('profession', 'text', array(
+                'required' => true,
+            ));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Morfeu\Bundle\EntityBundle\Entity\UserProfile'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'morfeu_bundle_userbundle_userprofile';
+    }
+}
