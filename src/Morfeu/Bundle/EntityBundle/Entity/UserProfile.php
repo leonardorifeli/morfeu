9d2724050a861f14a55038af139999ec39d4aece
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-11-08 22:57:57 -0200

    Developed a new method to get complete image

--- src/Morfeu/Bundle/EntityBundle/Entity/UserProfile.php
+++ src/Morfeu/Bundle/EntityBundle/Entity/UserProfile.php
@@ -3,73 +3,74 @@
 namespace Morfeu\Bundle\EntityBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Morfeu\Bundle\EntityBundle\Entity\User;
 
 /**
- * UserProfile
- *
- * @ORM\Table(name="user_profile", indexes={@ORM\Index(name="user_id", columns={"user_id"})})
- * @ORM\Entity
- */
-class UserProfile
+* UserProfile
+*
+* @ORM\Table(name="user_profile", indexes={@ORM\Index(name="user_id", columns={"user_id"})})
+* @ORM\Entity
+*/
+class UserProfile extends BaseEntity
 {
     /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer", precision=0, scale=0, nullable=false, unique=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
+    * @var integer
+    *
+    * @ORM\Column(name="id", type="integer", precision=0, scale=0, nullable=false, unique=false)
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="IDENTITY")
+    */
     private $id;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="photo", type="string", length=255, precision=0, scale=0, nullable=true, unique=false)
-     */
+    * @var string
+    *
+    * @ORM\Column(name="photo", type="string", length=255, precision=0, scale=0, nullable=true, unique=false)
+    */
     private $photo;
 
     /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="date_of_birth", type="date", precision=0, scale=0, nullable=true, unique=false)
-     */
+    * @var \DateTime
+    *
+    * @ORM\Column(name="date_of_birth", type="date", precision=0, scale=0, nullable=true, unique=false)
+    */
     private $dateOfBirth;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="profession", type="string", length=255, precision=0, scale=0, nullable=true, unique=false)
-     */
+    * @var string
+    *
+    * @ORM\Column(name="profession", type="string", length=255, precision=0, scale=0, nullable=true, unique=false)
+    */
     private $profession;
 
     /**
-     * @var \Morfeu\Bundle\EntityBundle\Entity\User
-     *
-     * @ORM\ManyToOne(targetEntity="Morfeu\Bundle\EntityBundle\Entity\User")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=true)
-     * })
-     */
+    * @var \Morfeu\Bundle\EntityBundle\Entity\User
+    *
+    * @ORM\ManyToOne(targetEntity="Morfeu\Bundle\EntityBundle\Entity\User")
+    * @ORM\JoinColumns({
+    *   @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=true)
+    * })
+    */
     private $user;
 
 
 
     /**
-     * Get id
-     *
-     * @return integer 
-     */
+    * Get id
+    *
+    * @return integer
+    */
     public function getId()
     {
         return $this->id;
     }
 
     /**
-     * Set photo
-     *
-     * @param string $photo
-     * @return UserProfile
-     */
+    * Set photo
+    *
+    * @param string $photo
+    * @return UserProfile
+    */
     public function setPhoto($photo)
     {
         $this->photo = $photo;
@@ -78,21 +79,21 @@ class UserProfile
     }
 
     /**
-     * Get photo
-     *
-     * @return string 
-     */
+    * Get photo
+    *
+    * @return string
+    */
     public function getPhoto()
     {
         return $this->photo;
     }
 
     /**
-     * Set dateOfBirth
-     *
-     * @param \DateTime $dateOfBirth
-     * @return UserProfile
-     */
+    * Set dateOfBirth
+    *
+    * @param \DateTime $dateOfBirth
+    * @return UserProfile
+    */
     public function setDateOfBirth($dateOfBirth)
     {
         $this->dateOfBirth = $dateOfBirth;
@@ -101,21 +102,21 @@ class UserProfile
     }
 
     /**
-     * Get dateOfBirth
-     *
-     * @return \DateTime 
-     */
+    * Get dateOfBirth
+    *
+    * @return \DateTime
+    */
     public function getDateOfBirth()
     {
         return $this->dateOfBirth;
     }
 
     /**
-     * Set profession
-     *
-     * @param string $profession
-     * @return UserProfile
-     */
+    * Set profession
+    *
+    * @param string $profession
+    * @return UserProfile
+    */
     public function setProfession($profession)
     {
         $this->profession = $profession;
@@ -124,21 +125,21 @@ class UserProfile
     }
 
     /**
-     * Get profession
-     *
-     * @return string 
-     */
+    * Get profession
+    *
+    * @return string
+    */
     public function getProfession()
     {
         return $this->profession;
     }
 
     /**
-     * Set user
-     *
-     * @param \Morfeu\Bundle\EntityBundle\Entity\User $user
-     * @return UserProfile
-     */
+    * Set user
+    *
+    * @param \Morfeu\Bundle\EntityBundle\Entity\User $user
+    * @return UserProfile
+    */
     public function setUser(\Morfeu\Bundle\EntityBundle\Entity\User $user = null)
     {
         $this->user = $user;
@@ -147,12 +148,20 @@ class UserProfile
     }
 
     /**
-     * Get user
-     *
-     * @return \Morfeu\Bundle\EntityBundle\Entity\User 
-     */
+    * Get user
+    *
+    * @return \Morfeu\Bundle\EntityBundle\Entity\User
+    */
     public function getUser()
     {
         return $this->user;
     }
+
+    public function getImage()
+    {
+        $image = $this->getImageComplete($this->photo);
+
+        return $image;
+    }
+
 }
