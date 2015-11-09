55f8b41bb8f0e3322d4d25a13a32b934069af574
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-11-08 22:56:50 -0200

    Changed the HttpStatus enum

---
+++ src/Morfeu/Bundle/BusinessBundle/Enum/HttpStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Morfeu\Bundle\BusinessBundle\Enum;
+
+use Morfeu\Bundle\BusinessBundle\Enum\BaseEnum;
+
+class HttpStatus extends BaseEnum
+{
+    const SUCCESS = 1;
+    const ERROR = 0;
+}
