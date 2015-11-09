67752b24678795d8a352f3766355d07d5fb17f36
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-11-09 01:35:58 -0200

    Created the counters and render that

--- src/Morfeu/Bundle/DashboardBundle/Controller/DashboardController.php
+++ src/Morfeu/Bundle/DashboardBundle/Controller/DashboardController.php
@@ -6,9 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DashboardController extends Controller
 {
+
+    private $dashboardService;
+
+    private function getDashboardService()
+    {
+        if(!$this->dashboardService){
+            $this->dashboardService = $this->get('dashboard.service');
+        }
+
+        return $this->dashboardService;
+    }
+
     public function indexAction()
     {
+        $user = $this->getUser();
+        $buzz = $this->container->get('buzz');
+        $actualPeriod = new \DateTime;
 
-        return $this->render('DashboardBundle:Dashboard:index.html.twig');
+        $totalBank = $this->getDashboardService()->getTotalBankByUser($user);
+        $totalCard = $this->getDashboardService()->getTotalCardByUser($user);
+        $dollar = $this->getDashboardService()->getCotationDollar($buzz);
+        $totalAccomplishedPayment = $this->getDashboardService()->getTotalAccomplishedByUserAndPeriod($user, null);
+        $totalAccomplishedPendingPayment = $this->getDashboardService()->getTotalAccomplishedPendingByUserAndPeriod($user, null);
+
+        return $this->render('DashboardBundle:Dashboard:index.html.twig', array(
+            'totalBank' => $totalBank,
+            'totalCard' => $totalCard,
+            'dollar' => $dollar,
+            'paymentAccomplishedTotal' => $totalAccomplishedPayment,
+            'paymentAccomplishedPendingTotal' => $totalAccomplishedPendingPayment,
+        ));
     }
 }
