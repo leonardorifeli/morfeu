daf4c7ffe3d2c17d45a9f0588722f0a1faee3d5f
Leonardo Rifeli <me@leonardorifeli.xyz>
2015-12-05 16:09:27 -0200

    Changed the enum class to abstract class

--- src/Morfeu/Bundle/BusinessBundle/Enum/FixedPayment.php
+++ src/Morfeu/Bundle/BusinessBundle/Enum/FixedPayment.php
@@ -4,7 +4,7 @@ namespace Morfeu\Bundle\BusinessBundle\Enum;
 
 use Morfeu\Bundle\BusinessBundle\Enum\BaseEnum;
 
-class FixedPayment extends BaseEnum
+abstract class FixedPayment extends BaseEnum
 {
 
     const YES = 1;
