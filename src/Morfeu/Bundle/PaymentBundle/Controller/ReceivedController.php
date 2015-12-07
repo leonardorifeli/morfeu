1b4fc7a411e8b3c5352c7fb3e2fb21f88b51ebd6
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-12-06 22:35:32 -0200

    Developed the filter in received controller

--- src/Morfeu/Bundle/PaymentBundle/Controller/ReceivedController.php
+++ src/Morfeu/Bundle/PaymentBundle/Controller/ReceivedController.php
@@ -3,6 +3,9 @@
 namespace Morfeu\Bundle\PaymentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Morfeu\Bundle\PaymentBundle\Form\PaymentFilterType;
+use Symfony\Component\HttpFoundation\Request;
+use Morfeu\Bundle\BusinessBundle\Model\PaymentFilter;
 
 class ReceivedController extends Controller
 {
@@ -21,11 +24,56 @@ class ReceivedController extends Controller
     {
         $user = $this->getUser();
 
-        $entities = $this->getPaymentService()->getReceivedByUserAndStatusAndPeriod($user);
+        $period = new \DateTime("now");
+        $period->setTime(0, 0);
+        $period->modify('first day of this month');
+
+        $periodTo = new \DateTime("now");
+        $periodTo->setTime(0, 0);
+        $periodTo->modify('last day of this month');
+
+        $entity = new PaymentFilter();
+        $filterForm = $this->createCreateFilterForm($entity);
+
+        $entities = $this->getPaymentService()->getReceivedByUserAndStatusAndPeriod($user, null, $period, $periodTo);
 
         return $this->render('PaymentBundle:Received:index.html.twig', array(
-            'entities' => $entities
+            'entities' => $entities,
+            'filterForm' => $filterForm->createView(),
         ));
+    }
+
+    public function filterAction(Request $request)
+    {
+        $user = $this->getUser();
+
+        $entity = new PaymentFilter();
+        $filterForm = $this->createCreateFilterForm($entity);
+        $filterForm->handleRequest($request);
+
+        $entities = $this->getPaymentService()->getReceivedByUserAndStatusAndPeriod($user, $entity->getStatus(), $entity->getPeriod(), $entity->getPeriodTo());
+
+        return $this->render('PaymentBundle:Received:index.html.twig', array(
+            'entities' => $entities,
+            'filterForm' => $filterForm->createView(),
+        ));
+    }
+
+    private function createCreateFilterForm(PaymentFilter $entity)
+    {
+        $form = $this->createForm(new PaymentFilterType(), $entity, array(
+            'action' => $this->generateUrl('payment_received_filter'),
+            'method' => 'GET',
+        ));
+
+        $form->add('submit', 'submit', array(
+            'label' => 'Filtrar',
+            'attr' => array(
+                'class'=>'btn btn-primary'
+            )
+        ));
+
+        return $form;
     }
 
 }
